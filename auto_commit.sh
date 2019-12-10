00d69d29bd6e197b4f529fb7e785b604eb529bb1
lsw1164 <qkqkfo@gmail.com>
2019-12-11 00:04:21 +0900

    Dummy commit with current time

---
+++ auto_commit.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+# Absolute path this script is in, thus /home/user/bin
+
+DIR_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+DUMMY_FILE="${DIR_PATH}/dummy"
+
+echo $DIR_PATH
+echo $DUMMY_FILE
+
+function update_current_date() {
+    OLD_LANG=$LANG
+    LANG="POSIX"
+    CURRENT_DATE=$(echo $(date))
+    LANG=$OLD_LANG 
+    echo "CURRENT_DATE : ${CURRENT_DATE}"
+}
+
+function make_dummy_change() {
+    if [ -f ${DUMMY_FILE} ]; then
+        rm ${DUMMY_FILE}
+    else 
+        touch ${DUMMY_FILE}
+    fi
+}
+
+function commit_with_current_date() {
+    git add ${DUMMY_FILE}
+    git commit -m "gradation" --date "${CURRENT_DATE}"
+}
+
+make_dummy_change
+update_current_date
+commit_with_current_date
