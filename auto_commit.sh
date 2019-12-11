7d508cce1d1c477f3e8e4822158103549880454f
lsw1164 <qkqkfo@gmail.com>
2019-12-11 23:57:02 +0900

    Need commit_count and day_ago options

--- auto_commit.sh
+++ auto_commit.sh
@@ -12,9 +12,49 @@
 
 DIR_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 DUMMY_FILE="${DIR_PATH}/dummy"
+COMMIT_CNT=0
+DAY_AGO=0
 
 echo $DIR_PATH
 echo $DUMMY_FILE
+
+#options
+#d - How many days ago
+#n - the number of commit count
+while getopts "n:d:" opt; do
+    case $opt in
+        d)
+            DAY_AGO=$(echo "${OPTARG}" | bc)
+            ;;
+        n)
+            COMMIT_CNT=$(echo "${OPTARG}" | bc)
+            ;;
+        \?) 
+            echo >&2 "ERR: Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo >&2 "ERR: Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+function validate_params() {
+    re='^[0-9]+$'
+    if ! [[ $COMMIT_CNT =~ $re ]] ; then
+        echo "ERR: -n option requires on a number argument."
+        exit 1
+    fi
+    if [[ $COMMIT_CNT -le 0 ]]; then
+        echo "ERR: -n option requires on a positive number argument."
+        exit 1
+    fi
+    if ! [[ $DAY_AGO =~ $re ]] ; then
+        echo "ERR: -d option requires on a number argument."
+        exit 1
+    fi
+}
 
 function update_current_date() {
     OLD_LANG=$LANG
@@ -37,6 +77,10 @@ function commit_with_current_date() {
     git commit -m "gradation" --date "${CURRENT_DATE}"
 }
 
-make_dummy_change
-update_current_date
-commit_with_current_date
+validate_params
+echo "COMMIT_CNT is ${COMMIT_CNT} DAY_AGO is ${DAY_AGO}"
+
+#make_dummy_change
+#update_current_date
+#commit_with_current_date
+
